0d319502f6a3e90d79789131c518880f58ae7c8b
Nick Wood <nickswift498@gmail.com>
2014-06-05 23:43:27 -0700

    adding some stuff for gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,12 @@
 
 /*
- *
+ * Michael Wood's Portfolio
+ * Gulpfile for development
  */
 
 var gulp   = require('gulp'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'); 
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin');
 
 gulp.task('default', []);
